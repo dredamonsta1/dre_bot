5544a1f7a2f39b05e19e4ca20adda51071d1b50f
andre wilkinson <andrewilkinson@andres-MacBook-Pro.local>
2019-01-02 23:25:48 -0800

    fixing error with old tweets being dups

--- the_kicks_bot.py
+++ the_kicks_bot.py
@@ -14,8 +14,26 @@ auth = tp.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tp.API(auth)
 
+FILE_NAME = 'last_seen_id.txt'
 
-mentions = api.mentions_timeline()
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+# last_seen_id = retrieve_last_seen_id(file_name)
+
+mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+
 for mention in mentions:
     print(str(mention.id) + ' - ' + mention.text)
     if '#helloworld' in mention.text.lower():
